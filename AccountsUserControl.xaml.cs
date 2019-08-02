6f7f7c2402816f9e72f230376d336eac34f7bd70
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-08-02 17:39:41 +0100

    feat(authentication): adds new sign-in window

--- AccountsUserControl.xaml.cs
+++ AccountsUserControl.xaml.cs
@@ -304,13 +304,13 @@ namespace SpecklePopup
   }
 
 
-  public class SpeckleAccount
-  {
-    public string email { get; set; }
-    public string apiToken { get; set; }
-    public string serverName { get; set; }
-    public string restApi { get; set; }
-    public string rootUrl { get; set; }
-    public bool isDefault { get; set; }
-  }
+  //public class SpeckleAccount
+  //{
+  //  public string email { get; set; }
+  //  public string apiToken { get; set; }
+  //  public string serverName { get; set; }
+  //  public string restApi { get; set; }
+  //  public string rootUrl { get; set; }
+  //  public bool isDefault { get; set; }
+  //}
 }
---
+++ SignInWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SpeckleCore;
+
+namespace SpecklePopup
+{
+  /// <summary>
+  /// Interaction logic for SignInWindow.xaml
+  /// </summary>
+  public partial class SignInWindow : Window
+  {
+
+    private string _defaultServer = "https://hestia.speckle.works/api";
+    private List<string> existingServers = new List<string>();
+    private List<string> existingServers_fullDetails = new List<string>();
+    internal ObservableCollection<Account> accounts = new ObservableCollection<Account>();
+    private bool validationCheckPass = false;
+    private Uri ServerAddress;
+    private string email;
+    private string password;
+    private string serverName;
+    public string restApi;
+    public string apitoken;
+
+    public SignInWindow( )
+    {
+      InitializeComponent();
+      LoadAccounts();
+    }
+
+
+    private void LoadAccounts( )
+    {
+      accounts = new ObservableCollection<Account>( LocalContext.GetAllAccounts() );
+      AccountListBox.ItemsSource = accounts;
+
+      if ( accounts.Any( x => x.IsDefault ) )
+      {
+        int index = accounts.Select( ( v, i ) => new { acc = v, index = i } ).First( x => x.acc.IsDefault ).index;
+        AccountListBox.SelectedIndex = index;
+      }
+    }
+
+
+    private void RadioButton_Click( object sender, RoutedEventArgs e )
+    {
+      var rb = sender as RadioButton;
+      LocalContext.SetDefaultAccount( rb.DataContext as Account );
+    }
+
+    private void AccountListBox_MouseDoubleClick( object sender, MouseButtonEventArgs e )
+    {
+      try
+      {
+        restApi = accounts[ AccountListBox.SelectedIndex ].RestApi;
+        apitoken = accounts[ AccountListBox.SelectedIndex ].Token;
+      } catch
+      {
+
+      }
+      //this.Close();
+    }
+  }
+}
