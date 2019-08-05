a777838043c5dc9d0cae1ddfc6915e4250a843bf
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-08-05 15:43:26 +0100

    feat(authentication): scaffolds browser & capture logic for user credentials

--- SignInWindow.xaml.cs
+++ SignInWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,7 +24,7 @@ namespace SpecklePopup
   public partial class SignInWindow : Window
   {
 
-    private string _defaultServer = "https://hestia.speckle.works/api";
+    private string defaultServer = "https://dev.hestia.speckle.works";
     private List<string> existingServers = new List<string>();
     private List<string> existingServers_fullDetails = new List<string>();
     internal ObservableCollection<Account> accounts = new ObservableCollection<Account>();
@@ -33,6 +35,8 @@ namespace SpecklePopup
     private string serverName;
     public string restApi;
     public string apitoken;
+
+    Process browser;
 
     public SignInWindow( )
     {
@@ -66,11 +70,52 @@ namespace SpecklePopup
       {
         restApi = accounts[ AccountListBox.SelectedIndex ].RestApi;
         apitoken = accounts[ AccountListBox.SelectedIndex ].Token;
-      } catch
+      }
+      catch
       {
 
       }
       //this.Close();
+    }
+
+    private void Button_Click( object sender, RoutedEventArgs e )
+    {
+      browser = Process.Start( "https://dev.hestia.speckle.works/signin?redirectUrl=http://localhost:5050" );
+      InstantiateWebServer();
+    }
+
+    private void InstantiateWebServer( )
+    {
+      HttpListener listener = new HttpListener();
+      listener.Prefixes.Add( "http://localhost:5050/" );
+      listener.Start();
+
+      var ctx = listener.GetContext();
+      var req = ctx.Request;
+      listener.Stop();
+
+      try
+      {
+        browser.CloseMainWindow();
+        browser.Close();
+      }
+      catch ( Exception e )
+      {
+        Debug.WriteLine( e );
+      }
+      var myString = Uri.UnescapeDataString( ctx.Request.Url.Query );
+
+      Debug.WriteLine( myString );
+      var splitRes = myString.Replace( "?token=", "" ).Split( new[ ] { ":::" }, StringSplitOptions.None );
+      var token = splitRes[ 0 ];
+      var serverUrl = splitRes[ 1 ];
+
+      var apiCl = new SpeckleApiClient( serverUrl + "/api" ) { AuthToken = token };
+      var res = apiCl.UserGetAsync().Result;
+
+      var apiToken = res.Resource.Apitoken;
+      var email = res.Resource.Email;
+      
     }
   }
 }
