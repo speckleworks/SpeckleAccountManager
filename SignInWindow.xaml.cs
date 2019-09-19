54e9e4032929b4da1f8fd92869734f3dd412a0e7
matteo@cominetti.org <matteo.cominetti@arup.com>
2019-09-19 08:13:33 +0100

    adds manual sign in from redirect url

--- SignInWindow.xaml.cs
+++ SignInWindow.xaml.cs
@@ -35,28 +35,28 @@ namespace SpecklePopup
     public string defaultServer
     {
       get { return _defaultServer; }
-      set { _defaultServer = value; OnPropertyChanged( "defaultServer" ); }
+      set { _defaultServer = value; OnPropertyChanged("defaultServer"); }
     }
 
     private bool _isCorrectUrl = true;
     public bool isCorrectUrl
     {
       get { return _isCorrectUrl; }
-      set { _isCorrectUrl = value; OnPropertyChanged( "isCorrectUrl" ); }
+      set { _isCorrectUrl = value; OnPropertyChanged("isCorrectUrl"); }
     }
 
     private string _errorMessage = "Url check result.";
     public string errorMessage
     {
       get { return _errorMessage; }
-      set { _errorMessage = value; OnPropertyChanged( "errorMessage" ); }
+      set { _errorMessage = value; OnPropertyChanged("errorMessage"); }
     }
 
     private bool _showSigninSuccess = false;
     public bool showSigninsuccess
     {
       get { return _showSigninSuccess; }
-      set { _showSigninSuccess = value; showMainLogin = !value; OnPropertyChanged( "showSigninsuccess" ); OnPropertyChanged( "showMainLogin" ); }
+      set { _showSigninSuccess = value; showMainLogin = !value; OnPropertyChanged("showSigninsuccess"); OnPropertyChanged("showMainLogin"); }
     }
 
     private bool _hasAccounts = false;
@@ -69,7 +69,7 @@ namespace SpecklePopup
     private bool _hasMultipleAccounts = false;
     public bool hasMultipleAccounts
     {
-      get { return accounts.Count>1; }
+      get { return accounts.Count > 1; }
       set { _hasMultipleAccounts = value; }
     }
 
@@ -82,7 +82,8 @@ namespace SpecklePopup
     public ObservableCollection<Account> accounts
     {
       get { return _accounts; }
-      set {
+      set
+      {
         _accounts = value; OnPropertyChanged("accounts");
       }
     }
@@ -99,7 +100,7 @@ namespace SpecklePopup
     //true if it's a popup wind0w. it will close automatically after selecting an account
     bool _isPopup;
 
-    public SignInWindow(bool isPopup = false )
+    public SignInWindow(bool isPopup = false)
     {
       this.DataContext = this;
 
@@ -110,11 +111,11 @@ namespace SpecklePopup
 
       LoadAccounts();
 
-      GetApiTimer = new Timer( 500 ) { Enabled = false, AutoReset = false };
+      GetApiTimer = new Timer(500) { Enabled = false, AutoReset = false };
       GetApiTimer.Elapsed += GetApiTimer_Elapsed;
       GetApiTimer.Start();
 
-     
+
     }
 
     private void Accounts_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
@@ -123,26 +124,26 @@ namespace SpecklePopup
       OnPropertyChanged("hasMultipleAccounts");
     }
 
-    private void GetApiTimer_Elapsed( object sender, ElapsedEventArgs e )
+    private void GetApiTimer_Elapsed(object sender, ElapsedEventArgs e)
     {
       try
       {
-        var baseUri = new Uri( defaultServer );
+        var baseUri = new Uri(defaultServer);
         var apiUri = baseUri.Scheme + "://" + baseUri.Host;
 
-        if ( !baseUri.IsDefaultPort ) { apiUri += ":" + baseUri.Port; }
+        if (!baseUri.IsDefaultPort) { apiUri += ":" + baseUri.Port; }
 
         apiUri += "/api";
 
-        var request = ( HttpWebRequest ) WebRequest.Create( new Uri( apiUri ) );
+        var request = (HttpWebRequest)WebRequest.Create(new Uri(apiUri));
         request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-        using ( HttpWebResponse response = ( HttpWebResponse ) request.GetResponse() )
-        using ( Stream stream = response.GetResponseStream() )
-        using ( StreamReader reader = new StreamReader( stream ) )
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
         {
           var test = reader.ReadToEnd();
           var tes2 = test;
-          if ( test.Contains( "isSpeckleServer" ) )
+          if (test.Contains("isSpeckleServer"))
           {
             isCorrectUrl = true;
             errorMessage = "Server url ok (got correct api response).";
@@ -154,7 +155,7 @@ namespace SpecklePopup
           }
         }
       }
-      catch ( Exception err )
+      catch (Exception err)
       {
         errorMessage = "There seems to be no speckle server there.";
         isCorrectUrl = false;
@@ -162,13 +163,13 @@ namespace SpecklePopup
     }
 
     public event PropertyChangedEventHandler PropertyChanged;
-    protected virtual void OnPropertyChanged( string propertyName )
+    protected virtual void OnPropertyChanged(string propertyName)
     {
-      PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
 
-    private void LoadAccounts( )
+    private void LoadAccounts()
     {
       var acc = LocalContext.GetAllAccounts();
       accounts.Clear();
@@ -177,26 +178,26 @@ namespace SpecklePopup
         accounts.Add(a);
       }
 
-      if ( accounts.Any( x => x.IsDefault ) )
+      if (accounts.Any(x => x.IsDefault))
       {
-        int index = accounts.Select( ( v, i ) => new { acc = v, index = i } ).First( x => x.acc.IsDefault ).index;
+        int index = accounts.Select((v, i) => new { acc = v, index = i }).First(x => x.acc.IsDefault).index;
         defaultAccountBox.SelectedIndex = index;
       }
     }
 
 
-    private void RadioButton_Click( object sender, RoutedEventArgs e )
+    private void RadioButton_Click(object sender, RoutedEventArgs e)
     {
       var rb = sender as RadioButton;
-      LocalContext.SetDefaultAccount( rb.DataContext as Account );
+      LocalContext.SetDefaultAccount(rb.DataContext as Account);
     }
 
-    private void AccountListBox_MouseDoubleClick( object sender, MouseButtonEventArgs e )
+    private void AccountListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
     {
       try
       {
-        restApi = accounts[ AccountListBox.SelectedIndex ].RestApi;
-        apitoken = accounts[ AccountListBox.SelectedIndex ].Token;
+        restApi = accounts[AccountListBox.SelectedIndex].RestApi;
+        apitoken = accounts[AccountListBox.SelectedIndex].Token;
       }
       catch
       {
@@ -205,35 +206,36 @@ namespace SpecklePopup
       //this.Close();
     }
 
-    private void SignInClick( object sender, RoutedEventArgs e )
+    private void SignInClick(object sender, RoutedEventArgs e)
     {
-      Task.Run( ( ) =>
-      {
-        var baseUri = new Uri( defaultServer );
-        var apiUri = baseUri.Scheme + "://" + baseUri.Host;
+      Task.Run(() =>
+     {
+       var baseUri = new Uri(defaultServer);
+       var apiUri = baseUri.Scheme + "://" + baseUri.Host;
 
-        if ( !baseUri.IsDefaultPort ) { apiUri += ":" + baseUri.Port; }
+       if (!baseUri.IsDefaultPort) { apiUri += ":" + baseUri.Port; }
 
-        browser = Process.Start( this.defaultServer + "/signin?redirectUrl=http://localhost:5050" );
-        isInRequestFlow = true;
+       browser = Process.Start(this.defaultServer + "/signin?redirectUrl=http://localhost:5050");
+       isInRequestFlow = true;
 
-        InstantiateWebServer();
-      } );
+       InstantiateWebServer();
+     });
     }
 
-    private void InstantiateWebServer( )
+    private void InstantiateWebServer()
     {
-      if ( listener != null )
+      if (listener != null)
       {
         listener.Abort();
       }
       try
       {
         listener = new HttpListener();
-        listener.Prefixes.Add( "http://localhost:5050/" );
+        listener.Prefixes.Add("http://localhost:5050/");
         listener.Start();
 
         var ctx = listener.GetContext();
+        //this is where it stops to wait for a request
         var req = ctx.Request;
         listener.Stop();
 
@@ -242,77 +244,85 @@ namespace SpecklePopup
           browser.CloseMainWindow();
           browser.Close();
         }
-        catch ( Exception e )
+        catch (Exception e)
         {
-          Debug.WriteLine( e );
+          Debug.WriteLine(e);
         }
-        var myString = Uri.UnescapeDataString( ctx.Request.Url.Query );
 
-        var splitRes = myString.Replace( "?token=", "" ).Split( new[ ] { ":::" }, StringSplitOptions.None );
-        var token = splitRes[ 0 ];
-        var serverUrl = splitRes[ 1 ];
+        AddAccountFromRedirect(ctx.Request.Url.Query);
 
-        var apiCl = new SpeckleApiClient( serverUrl + "/api" ) { AuthToken = token };
-        var res = apiCl.UserGetAsync().Result;
 
-        var apiToken = res.Resource.Apitoken;
-        var email = res.Resource.Email;
-
-        SaveOrUpdateAccount( new Account() { RestApi = apiCl.BaseUrl, Email = email, Token = apiToken } );
-
-        Dispatcher.Invoke( ( ) =>
-        {
-          defaultServer = "";
-          showSigninsuccess = true;
-          isInRequestFlow = false;
-          LoadAccounts();
-        } );
       }
-      catch ( Exception e )
+      catch (Exception e)
       {
-        Debug.WriteLine( e );
+        Debug.WriteLine(e);
       }
     }
 
-    public void SaveOrUpdateAccount( Account newAccount )
+    private void AddAccountFromRedirect(string redirect)
+    {
+      var myString = Uri.UnescapeDataString(redirect);
+
+      var splitRes = myString.Replace("?token=", "").Split(new[] { ":::" }, StringSplitOptions.None);
+      var token = splitRes[0];
+      var serverUrl = splitRes[1];
+
+      var apiCl = new SpeckleApiClient(serverUrl + "/api") { AuthToken = token };
+      var res = apiCl.UserGetAsync().Result;
+
+      var apiToken = res.Resource.Apitoken;
+      var email = res.Resource.Email;
+
+      SaveOrUpdateAccount(new Account() { RestApi = apiCl.BaseUrl, Email = email, Token = apiToken });
+
+      Dispatcher.Invoke(() =>
+      {
+        //defaultServer = "";
+        showSigninsuccess = true;
+        isInRequestFlow = false;
+        LoadAccounts();
+      });
+    }
+
+    public void SaveOrUpdateAccount(Account newAccount)
     {
       var existingAccounts = LocalContext.GetAllAccounts();
-      var newUri = new Uri( newAccount.RestApi );
+      var newUri = new Uri(newAccount.RestApi);
 
-      var serverName = GetServerName( newUri );
+      var serverName = GetServerName(newUri);
 
-      foreach ( var acc in existingAccounts )
+      foreach (var acc in existingAccounts)
       {
-        var eUri = new Uri( acc.RestApi );
-        if ( ( eUri.Host == newUri.Host ) && ( acc.Email == newAccount.Email ) && ( eUri.Port == newUri.Port ) )
+        var eUri = new Uri(acc.RestApi);
+        if ((eUri.Host == newUri.Host) && (acc.Email == newAccount.Email) && (eUri.Port == newUri.Port))
         {
           acc.ServerName = serverName;
           acc.Token = newAccount.Token;
-          LocalContext.RemoveAccount( acc ); // TODO: Add update account method, as this is rather stupid
-          LocalContext.AddAccount( acc );
+          LocalContext.RemoveAccount(acc); // TODO: Add update account method, as this is rather stupid
+          LocalContext.AddAccount(acc);
           return;
         }
       }
       newAccount.ServerName = serverName;
-      LocalContext.AddAccount( newAccount );
+      LocalContext.AddAccount(newAccount);
     }
 
-    public string GetServerName( Uri serverApi )
+    public string GetServerName(Uri serverApi)
     {
-      using ( var cl = new WebClient() )
+      using (var cl = new WebClient())
       {
-        var response = JsonConvert.DeserializeObject<dynamic>( cl.DownloadString( serverApi ) );
+        var response = JsonConvert.DeserializeObject<dynamic>(cl.DownloadString(serverApi));
         return Convert.ToString(response.serverName);
       }
-      throw new Exception( "Could not get server name." );
+      throw new Exception("Could not get server name.");
     }
 
-    private void serverUrlTextChanged( object sender, TextChangedEventArgs e )
+    private void serverUrlTextChanged(object sender, TextChangedEventArgs e)
     {
-      defaultServer = ( ( TextBox ) sender ).Text;
+      defaultServer = ((TextBox)sender).Text;
       try
       {
-        var testUri = new Uri( defaultServer );
+        var testUri = new Uri(defaultServer);
         GetApiTimer.Start();
       }
       catch
@@ -322,7 +332,7 @@ namespace SpecklePopup
       }
     }
 
-    private void ReturnToMain( object sender, RoutedEventArgs e )
+    private void ReturnToMain(object sender, RoutedEventArgs e)
     {
       showSigninsuccess = false;
     }
@@ -358,9 +368,27 @@ namespace SpecklePopup
 
     private void AccountListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-      if(AccountListBox.SelectedIndex !=-1)
+      if (AccountListBox.SelectedIndex != -1)
       {
         this.Close();
+      }
+    }
+
+    private void ManuallyAddAccountClick(object sender, RoutedEventArgs e)
+    {
+      var win = new ConnectionStringWin();
+      var result = win.ShowDialog();
+      if (result.HasValue && result.Value && !string.IsNullOrEmpty(win.connectionString.Text))
+      {
+        try
+        {
+          AddAccountFromRedirect(win.connectionString.Text);
+        }
+        catch (Exception ex)
+        {
+          Debug.WriteLine(ex);
+        }
+
       }
     }
   }
